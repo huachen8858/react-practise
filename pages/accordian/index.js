22aa9381f95f13b8262bc105f208bbca35d67357
huachen8858 <huachen8858@gmail.com>
2024-02-14 15:24:48 +0800

    multiple section toggle

--- pages/accordian/index.js
+++ pages/accordian/index.js
@@ -4,26 +4,69 @@ import accordianData from "@/data/accordian-data";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   const handleSingleSelection = (currentId) => {
     // console.log(currentId);
     setSelected(currentId === selected ? null : currentId);
   };
+
+  const handleMultiSelection = (currentId) => {
+    let copyMutiple = [...multiple];
+    // 找出點擊的item index
+    const findIndextOfCurrentId = copyMutiple.indexOf(currentId);
+    // 如果沒找到就添加陣列的展開複製的 copyMutiple
+    if (findIndextOfCurrentId === -1) copyMutiple.push(currentId);
+    // 如果有就移除
+    else copyMutiple.splice(findIndextOfCurrentId, 1);
+
+    setMultiple(copyMutiple);
+  };
+
+  // console.log(selected, multiple);
+
   return (
     <div className={styles["wrapper"]}>
-    <h1 className={styles['page-title']}>Accordian</h1>
-      <div className={styles['accordian']}>
+      <h1 className={styles["page-title"]}>01 Accordian</h1>
+      <button
+        onClick={() => {
+          setEnableMultiSelection(!enableMultiSelection);
+        }}
+      >
+        Enable Multi Selection
+      </button>
+      <div className={styles["accordian"]}>
         {accordianData && accordianData.length > 0 ? (
           accordianData.map((dataItem) => (
             <div className={styles["acc-item"]} key={dataItem.id}>
               <div
                 className={styles["acc-title"]}
-                onClick={() => handleSingleSelection(dataItem.id)}
+                onClick={
+                  enableMultiSelection
+                    ? () => {
+                        handleMultiSelection(dataItem.id);
+                      }
+                    : () => handleSingleSelection(dataItem.id)
+                }
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id ? <div className={styles['acc-content']}>{dataItem.answer}</div> : null}
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className={styles["acc-content"]}>
+                      {dataItem.answer}
+                    </div>
+                  )
+                : selected === dataItem.id && (
+                    <div className={styles["acc-content"]}>
+                      {dataItem.answer}
+                    </div>
+                  )}
+              {/* {selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className={styles["acc-content"]}>{dataItem.answer}</div>
+              ) : null} */}
             </div>
           ))
         ) : (
